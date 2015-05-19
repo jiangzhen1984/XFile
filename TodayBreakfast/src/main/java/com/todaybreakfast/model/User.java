299faa251069aee382f8782c13f5e6d679b721da
Jiang Hunk <hunk.jiang@sonymobile.com>
2015-05-19 10:06:39 +0800

    Add hibernate build dependencies.

---
+++ TodayBreakfast/src/main/java/com/todaybreakfast/model/User.java
@@ -0,0 +1,74 @@
+package com.todaybreakfast.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+
+@Entity
+@Table( name = "TB_USER" )
+public class User {
+	
+	
+	@Id
+	@GeneratedValue(generator="increment")
+	@GenericGenerator(name="increment", strategy = "increment")
+	private long id;
+	
+	private String cellPhone;
+	
+	private String password;
+	
+	private String name;
+	
+	private String address;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCellPhone() {
+		return cellPhone;
+	}
+
+	public void setCellPhone(String cellPhone) {
+		this.cellPhone = cellPhone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+	
+	
+
+}
